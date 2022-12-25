8ce4267be496c6a2eabf26aef60f991ed1c489e1
ali abedi <ali.abedi9731@gmail.com>
2022-12-25 18:53:29 +0330

    messed up in migrations cleaned and some dictation mistakes fixed from database and migrations and added notification table to the projcet migrations

--- database/migrations/2022_12_25_184151_create_notifications_table.php
+++ database/migrations/2022_12_25_184151_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSlideshowTable extends Migration
+class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateSlideshowTable extends Migration
      */
     public function up()
     {
-        Schema::create('slideshow', function (Blueprint $table) {
-            $table->id();
-            $table->text('image');
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -28,6 +30,6 @@ class CreateSlideshowTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('slideshow');
+        Schema::dropIfExists('notifications');
     }
 }
