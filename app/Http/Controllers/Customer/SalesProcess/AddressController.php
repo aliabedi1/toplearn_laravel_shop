bf8aa9a06a70ba75f33799003f1d3e2023624dcd
Ali <MY_NAME@example.com>
2023-08-24 16:35:45 +0330

    address controller completed with edit and adding address

--- app/Http/Controllers/Customer/SalesProcess/AddressController.php
+++ app/Http/Controllers/Customer/SalesProcess/AddressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer\SalesProcess;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\Address\AddressRequest;
 use App\Models\Address;
 use App\Models\Market\CartItem;
 use App\Models\Province;
@@ -27,14 +28,29 @@ class AddressController extends Controller
     }
 
 
-    public function addAddress( $request)
+    public function addAddress(AddressRequest $request)
     {
-        
+        Address::create($this->makeAddressValues($request->all()));
+        return redirect()->route('customer.sales-process.address-and-delivery')->with('toast-success','ادرس با موفقیت اضافه شد');
     }
 
-    public function editAddress( $request, Address $address)
+    public function editAddress(AddressRequest $request, Address $address)
     {
-        
+        $address->update($this->makeAddressValues($request->all()));
+        return redirect()->route('customer.sales-process.address-and-delivery')->with('toast-success','ادرس با موفقیت ویرایش شد');    
+    }
+
+    public function makeAddressValues($inputs)
+    {
+        if($inputs['i_am_recipient'] == 1)
+        {
+            $inputs['first_name'] = auth()->user()->first_name;
+            $inputs['last_name'] = auth()->user()->last_name;
+            $inputs['mobile'] = auth()->user()->mobile;
+        }
+        unset($inputs['i_am_recipient']);
+
+        return $inputs;
     }
 
 }
